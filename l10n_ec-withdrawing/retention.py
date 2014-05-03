3498c5171f1758571d98c6a736f94f6f873dbe17
Cristian Salamea <ovnicraft@gmail.com>
2014-05-02 21:58:54 -0500

    cambio en account_invoice_tax

--- l10n_ec-withdrawing/retention.py
+++ l10n_ec-withdrawing/retention.py
@@ -257,8 +257,6 @@ class account_retention(osv.osv):
         self.write(cr, uid, ids, {'state': 'early'})
         return True        
 
-account_retention()
-
 
 class account_invoice_tax(osv.osv):
 
@@ -289,20 +287,19 @@ class account_invoice_tax(osv.osv):
         cur_obj = self.pool.get('res.currency')
         inv = self.pool.get('account.invoice').browse(cr, uid, invoice_id, context=context)
         cur = inv.currency_id
-        company_currency = inv.company_id.currency_id.id
-
+        company_currency = self.pool['res.company'].browse(cr, uid, inv.company_id.id).currency_id.id
         for line in inv.invoice_line:
-            for tax in tax_obj.compute_all(cr, uid, line.invoice_line_tax_id, (line.price_unit* (1-(line.discount or 0.0)/100.0)), line.quantity, inv.address_invoice_id.id, line.product_id, inv.partner_id)['taxes']:
+            for tax in tax_obj.compute_all(cr, uid, line.invoice_line_tax_id, (line.price_unit* (1-(line.discount or 0.0)/100.0)), line.quantity, line.product_id, inv.partner_id)['taxes']:
                 val={}
                 tax_group = self.pool.get('account.tax').read(cr, uid, tax['id'],['tax_group', 'amount', 'description'])
                 val['invoice_id'] = inv.id
                 val['name'] = tax['name']
-                val['tax_group'] = tax_group['tax_group']
-                val['percent'] = tax_group['description']
                 val['amount'] = tax['amount']
+                val['tax_group'] = tax_group['tax_group']
+                val['percent'] = tax_group['description']                
                 val['manual'] = False
                 val['sequence'] = tax['sequence']
-                val['base'] = tax['price_unit'] * line['quantity']
+                val['base'] = cur_obj.round(cr, uid, cur, tax['price_unit'] * line['quantity'])
                 if tax_group['tax_group'] in ['ret_vat_b', 'ret_vat_srv']:
                     ret = float(str(tax_group['description'])) / 100
                     bi = tax['price_unit'] * line['quantity']
@@ -310,20 +307,22 @@ class account_invoice_tax(osv.osv):
                     val['base'] = (tax['price_unit'] * line['quantity']) * imp / 100
                 else:
                     val['base'] = tax['price_unit'] * line['quantity']
-                if inv.type in ('out_invoice','in_invoice', 'liq_purchase'):
+                if inv.type in ('out_invoice','in_invoice','liq_purchase'):
                     val['base_code_id'] = tax['base_code_id']
                     val['tax_code_id'] = tax['tax_code_id']
                     val['base_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['base'] * tax['base_sign'], context={'date': inv.date_invoice or time.strftime('%Y-%m-%d')}, round=False)
                     val['tax_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['amount'] * tax['tax_sign'], context={'date': inv.date_invoice or time.strftime('%Y-%m-%d')}, round=False)
                     val['account_id'] = tax['account_collected_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_collected_id']
                 else:
                     val['base_code_id'] = tax['ref_base_code_id']
                     val['tax_code_id'] = tax['ref_tax_code_id']
                     val['base_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['base'] * tax['ref_base_sign'], context={'date': inv.date_invoice or time.strftime('%Y-%m-%d')}, round=False)
                     val['tax_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['amount'] * tax['ref_tax_sign'], context={'date': inv.date_invoice or time.strftime('%Y-%m-%d')}, round=False)
                     val['account_id'] = tax['account_paid_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_paid_id']
 
-                key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
+                key = (val['tax_code_id'], val['base_code_id'], val['account_id'], val['account_analytic_id'])
                 if not key in tax_grouped:
                     tax_grouped[key] = val
                 else:
@@ -339,31 +338,9 @@ class account_invoice_tax(osv.osv):
             t['tax_amount'] = cur_obj.round(cr, uid, cur, t['tax_amount'])
         return tax_grouped
 
-    def move_line_get(self, cr, uid, invoice_id):
-        res = []
-        cr.execute('SELECT * FROM account_invoice_tax WHERE invoice_id=%s', (invoice_id,))
-        for t in cr.dictfetchall():
-            if not t['amount'] \
-                    and not t['tax_code_id'] \
-                    and not t['tax_amount']:
-                continue
-            res.append({
-                'type':'tax',
-                'name':t['name'],
-                'price_unit': t['amount'],
-                'quantity': 1,
-                'price': t['amount'] or 0.0,
-                'account_id': t['account_id'],
-                'tax_code_id': t['tax_code_id'],
-                'tax_amount': t['tax_amount']
-            })
-        return res
-
     _defaults = dict(
         fiscal_year = time.strftime('%Y'),
     )    
-
-account_invoice_tax()
 
 
 class Invoice(osv.osv):
