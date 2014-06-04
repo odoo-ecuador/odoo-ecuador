0b73cec2fc62775a823ffd05f11de22c6f9dc6c0
Cristian Salamea <cristian@ayni.io>
2014-06-04 14:54:29 -0500

    WIP Modulo de reportes y anticipos
    Los módulos para reporte de Flujo de Efectivo y modulo para anticipos

---
+++ l10n_ec_reports/__openerp__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 Cristian Salamea.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'Basic Account Reportes',
+    'version' : '0.1.0',
+    'author' : 'Cristian Salamea',
+    'category': 'Accounting',
+    'complexity': 'normal',
+    'website': 'http://www.ayni.io',
+    'data': [
+        'security/l10n_ec_reports_security.xml',
+        'security/ir.model.access.csv',
+        'view/l10n_ec_reports_view.xml',
+        'report/l10n_ec_reports_report.xml',
+        'data/l10n_ec_reports_data.xml',
+    ],
+    'depends' : [
+    ],
+    'js': [
+    ],
+    'qweb': [
+    ],
+    'css': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,                
+}
