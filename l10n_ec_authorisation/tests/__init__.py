89fc0acf35b71a422ed717d5584c76bbbe982b45
Cristian Salamea <cristian@ayni.io>
2014-06-08 17:07:39 -0500

    agregado pruebas para modulo

---
+++ l10n_ec_authorisation/tests/__init__.py
@@ -0,0 +1,5 @@
+from . import test_authorisation
+
+fast_suite = [
+    test_authorisation
+]
---
+++ l10n_ec_authorisation/tests/test_authorisation.py
@@ -0,0 +1,55 @@
+from openerp.tests.common import TransactionCase
+
+import time
+
+class TestAuthorisation(TransactionCase):
+    """
+    Prueba de documentos de autorizacion
+    """
+    def setUp(self):
+        super(TestAuthorisation, self).setUp()
+        self.Authorisation = self.registry('account.authorisation')
+        self.Partner = self.registry('res.partner')
+        self.AtsDoc = self.registry('account.ats.doc')
+
+    def test_create(self):
+        cursor = self.cr
+        user_id = self.uid
+
+        type_id = self.AtsDoc.create(
+            cursor,
+            user_id,
+            {
+                'code': '06',
+                'name': 'FACTURA'
+            }
+        )
+
+        partner_id = self.Partner.create(
+            cursor,
+            user_id,
+            {
+                'ced_ruc': '0103893962',
+                'name': 'CRISTIAN GONZALO SALAMEA MALDONADO',
+                'type_ced_ruc': 'cedula',
+                'tipo_persona': '6'
+            }
+        )
+        
+        auth_id = self.Authorisation.create(
+            cursor,
+            user_id,
+            {
+                'name': '0123456789',
+                'serie_entidad': '001',
+                'serie_emision': '001',
+                'num_start': 1000,
+                'num_end': 3000,
+                'expiration_date': time.strftime('%Y-%m-%d'),
+                'in_type': 'interno',
+                'type_id': type_id,
+                'partner_id': partner_id
+            }
+        )
+
+        self.assertNotEquals(auth_id, 0)
