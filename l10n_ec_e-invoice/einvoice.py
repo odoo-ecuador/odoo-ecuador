7aedc9b9d8839995322922ec255687f1546776f3
Cristian Salamea <cristian@ayni.io>
2015-01-09 00:47:27 -0500

    Metodo de firma
    creado un método de firma para cualquier implementacion.
    en base al XML

--- l10n_ec_e-invoice/einvoice.py
+++ l10n_ec_e-invoice/einvoice.py
@@ -276,8 +276,27 @@ class AccountInvoice(osv.osv):
             xmlschema.assertValid(factura)
         except DocumentInvalid as e:
             raise osv.except_osv('Error de Datos', """El sistema generó el XML pero la factura no pasa la validación XSD del SRI.
-            \nLos errores mas comunes son:\n* RUC,Cédula o Pasaporte contiene caracteres no válidos.\n* Números de documentos están duplicados.\n\nEl siguiente error contiene el identificador o número de documento en conflicto:\n\n %s""" % str(e))        
-        
+            \nLos errores mas comunes son:\n* RUC,Cédula o Pasaporte contiene caracteres no válidos.\n* Números de documentos están duplicados.\n\nEl siguiente error contiene el identificador o número de documento en conflicto:\n\n %s""" % str(e))
+
+    def apply_digital_signature(self, cr, uid, factura):
+        """
+        Metodo que aplica la firma digital al XML
+        """
+        ds_document = False
+        tree = etree.ElementTree(factura)
+        name = '%s%s.xml' %('/opt/facturas/', claveAcceso)
+        tree.write(name, pretty_print=True, xml_declaration=True, encoding='utf-8', method="xml")
+        #firma electrónica del xml
+        firma_path = os.path.join(os.path.dirname(__file__), 'firma/prctXadesBes.jar')
+        #CLINICA METROPOLITANA
+        file_pk12 = 'L29wdC9jZXJ0aWZpY2Fkb3Mvam9yZ2VfamF2aWVyX2ppcm9uX3Jvc2Vyby5wMTI='
+        password = 'eHRwcTg4MDBNZXRybw=='
+        #PEDRO RODRIGUEZ
+        #file_pk12 = 'L29wdC9jZXJ0aWZpY2Fkb3MvcGVkcm9fYWx2YXJvX3JvZHJpZ3Vlel9yb3Nlcm8ucDEy'
+        #password = 'UENCNzMxOWFndWlsYQ=='
+        #invocación del jar de la firma electrónica
+        subprocess.call(['java', '-jar', firma_path, name, name, file_pk12, password])
+        return ds_document
     
     def action_generate_einvoice(self, cr, uid, ids, context=None):
         """
@@ -299,20 +318,8 @@ class AccountInvoice(osv.osv):
             #validación del xml
             self.validate_xml(cr, uid, factura)
 
-            #grabación del xml en el disco
-            tree = etree.ElementTree(factura)
-            name = '%s%s.xml' %('/opt/facturas/', claveAcceso)
-            tree.write(name,pretty_print=True,xml_declaration=True,encoding='utf-8',method="xml")
-            #firma electrónica del xml
-            firma_path = os.path.join(os.path.dirname(__file__), 'firma/prctXadesBes.jar')
-            #CLINICA METROPOLITANA
-            file_pk12 = 'L29wdC9jZXJ0aWZpY2Fkb3Mvam9yZ2VfamF2aWVyX2ppcm9uX3Jvc2Vyby5wMTI='
-            password = 'eHRwcTg4MDBNZXRybw=='
-            #PEDRO RODRIGUEZ
-            #file_pk12 = 'L29wdC9jZXJ0aWZpY2Fkb3MvcGVkcm9fYWx2YXJvX3JvZHJpZ3Vlel9yb3Nlcm8ucDEy'
-            #password = 'UENCNzMxOWFndWlsYQ=='
-            #invocación del jar de la firma electrónica
-            subprocess.call(['java', '-jar', firma_path, name, name, file_pk12, password])
+            # firma de XML, now what ??
+            ds_invoice = self.apply_digital_signature(cr, uid, factura)
             
         return True
 
