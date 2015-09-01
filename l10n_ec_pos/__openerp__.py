b9c7e0f690b3f43cbdf22dc4cf9ac3c433735ebc
Cristian Salamea <ovnicraft@gmail.com>
2015-09-01 00:39:07 -0500

    Draft modulo Partner POS

---
+++ l10n_ec_pos/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ec_pos",
+
+    'summary': """
+    Cambios en el POS para Ecuador
+    """,
+
+    'description': """
+        Agregar campos del partner requeridos en el POS
+    """,
+
+    'author': "Cristian Salamea",
+    'website': "http://www.coopayni.com",
+
+    'category': 'POS',
+    'version': '0.1',
+
+    'depends': ['l10n_ec_partner'],
+
+    'data': [
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml'
+        ],
+    'demo': [
+    ],
+}
