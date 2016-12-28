e56b76ab43bb7820bb9f9dbb37ced8582769f193
alcidesrivera <alcides.rivera84@gmail.com>
2016-12-28 16:15:52 -0500

    Clean code in l10n_ec_ote (#155)

--- l10n_ec_ote/country.py
+++ l10n_ec_ote/country.py
@@ -2,23 +2,30 @@
 # © <2016> <Alcides Rivera VIRTUALSAMI CIA LTDA>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import fields, osv
+from openerp import models, fields
 
-
-class CountryCity(osv.osv):
+class CountryCity(models.Model):
+  
     _name = 'res.country.city'
     _description = "City model"
+    
+    country_id = fields.Many2one(
+        'res.country', 'Country',
+        required=True
+        )
+    state_id = fields.Many2one(
+        'res.country.state', 'State',
+        domain="[('country_id','=',country_id)]",
+        required=True
+        )
+    name = fields.Char(
+        'Name', 
+        size=64, 
+        required=True
+        )
+    code = fields.Char(
+        'Code', 
+        size=4, 
+        required=True
+        )
 
-    _columns = {
-        'country_id': fields.many2one(
-            'res.country', 'Country',
-            required=True
-        ),
-        'state_id': fields.many2one(
-            'res.country.state', 'State',
-            domain="[('country_id','=',country_id)]",
-            required=True
-        ),
-        'name': fields.char('Name', size=64, required=True),
-        'code': fields.char('Code', size=4, required=True),
-    }
